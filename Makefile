d3010327c559d78bc78806f2296ae49efbffe9f8
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2020-04-19 17:30:47 +0900

    Implement benchmark

---
+++ Makefile
@@ -0,0 +1,14 @@
+GRADLE = ./gradlew --no-daemon
+BENCH = java -jar build/libs/croppng-*-jmh.jar
+
+BENCH_OPTS = -f $(NUM_FORKS) -tu $(TIME_UNIT) -rf $(RESULT_FORMAT)
+NUM_FORKS = 5
+TIME_UNIT = 's'
+RESULT_FORMAT = 'csv'
+
+bench:
+	$(GRADLE) clean jmhJar
+	sleep 10
+	$(BENCH) $(BENCH_OPTS) -t 1 -rff 'benchmark-result-t1.csv' >benchmark-console-t1.txt
+	sleep 60
+	$(BENCH) $(BENCH_OPTS) -t 40 -rff 'benchmark-result-t40.csv' >benchmark-console-t40.txt
---
+++ src/jmh/java/me/k11i/croppng/CropPngBenchmark.java
@@ -0,0 +1,76 @@
+package me.k11i.croppng;
+
+import me.k11i.croppng.test.helper.CropParam;
+import me.k11i.croppng.test.helper.JavaIoImageCrop;
+import me.k11i.croppng.test.helper.TestImage;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.SplittableRandom;
+
+@State(Scope.Thread)
+public class CropPngBenchmark {
+    @State(Scope.Thread)
+    public static class BenchmarkContext {
+        private boolean firstPass = true;
+        private byte[][] testImageBytes;
+        private SplittableRandom random;
+        private Iterator<CropParam> cropParamItr;
+
+        private byte[] src;
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+        private int scaleFactor;
+
+        @Setup(Level.Invocation)
+        public void setUp() {
+            if (firstPass) {
+                TestImage testImage = TestImage.TILE;
+                testImageBytes = testImage.loadImages();
+                random = new SplittableRandom(1);
+                cropParamItr = testImage.randomCropParameters(random).iterator();
+                firstPass = false;
+            }
+
+            src = testImageBytes[random.nextInt(5)];
+            CropParam cropParam = cropParamItr.next();
+            x = cropParam.x;
+            y = cropParam.y;
+            width = cropParam.width;
+            height = cropParam.height;
+            scaleFactor = cropParam.scaleFactor;
+        }
+    }
+
+    @Benchmark
+    public byte[] cropPng6(BenchmarkContext ctx) {
+        var result = new CropPng().crop(ctx.src, ctx.x, ctx.y, ctx.width, ctx.height, ctx.scaleFactor);
+        return Arrays.copyOfRange(result.array(), result.arrayOffset(), result.limit());
+    }
+
+    @Benchmark
+    public byte[] cropPng6ThreadLocalSoftRef(BenchmarkContext ctx) {
+        var result = CropPng.defaultLevel().crop(ctx.src, ctx.x, ctx.y, ctx.width, ctx.height, ctx.scaleFactor);
+        return Arrays.copyOfRange(result.array(), result.arrayOffset(), result.limit());
+    }
+
+    @Benchmark
+    public byte[] cropPng1(BenchmarkContext ctx) {
+        var result = new CropPng(1).crop(ctx.src, ctx.x, ctx.y, ctx.width, ctx.height, ctx.scaleFactor);
+        return Arrays.copyOfRange(result.array(), result.arrayOffset(), result.limit());
+    }
+
+    @Benchmark
+    public  byte[] cropPng1ThreadLocalSoftRef(BenchmarkContext ctx) {
+        var result = CropPng.compressionLevel(1).crop(ctx.src, ctx.x, ctx.y, ctx.width, ctx.height, ctx.scaleFactor);
+        return Arrays.copyOfRange(result.array(), result.arrayOffset(), result.limit());
+    }
+
+    @Benchmark
+    public byte[] javaIoImage(BenchmarkContext ctx) {
+        return JavaIoImageCrop.crop(ctx.src, ctx.x, ctx.y, ctx.width, ctx.height, ctx.scaleFactor);
+    }
+}
